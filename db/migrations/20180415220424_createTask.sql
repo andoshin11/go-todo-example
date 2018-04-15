7792ca0b50f062aac3e46de72eaf0450eceb1a97
andoshin11 <shinglish11@gmail.com>
2018-04-15 22:23:42 +0900

    Create Task table

---
+++ db/migrations/20180415220424_createTask.sql
@@ -0,0 +1,15 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE IF NOT EXISTS task (
+	id INT UNSIGNED NOT NULL,
+	created_at TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+	updated_at TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+	title VARCHAR(255),
+	PRIMARY KEY(id)
+);
+
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE task;
